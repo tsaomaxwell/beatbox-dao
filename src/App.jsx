852979bef61bf69da2aaa0a1a54c59f26b6fda9b
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2021-12-19 22:35:48 -0800

    thirdweb environment setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useMemo, useState } from "react";
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("Address:", address);
+  if(!address){
+    return (
+      <div className="landing">
+        <h1>Welcome to BeatboxDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>(s) B t (pf)</h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
+// 4 => rinkeby
+const supportedChainIds = [4];
+// metamask is injected
+const connectors = {
+  injected: {},
+};
+
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <div className="landing">
+        <App />
+      </div>
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
